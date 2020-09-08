7bde6f6806d9e6bddd6c4f9349d499c7ace359f1
mkizka <30231179+mkizka@users.noreply.github.com>
2020-09-08 11:36:51 +0900

    markdown変換の実装

--- services/renderer-ts/src/renderer.ts
+++ services/renderer-ts/src/renderer.ts
@@ -1,8 +1,13 @@
+import unified from "unified";
+import markdown from "remark-parse";
+import remark2rehype from "remark-rehype";
+import html from "rehype-stringify";
+
 /**
  * 受け取った文書を HTML に変換する
  */
 export async function render(src: string): Promise<string> {
-  // TODO: これはサンプル実装 (URL の自動リンク) です
-  const html = src.replace(/https?:\/\/[^\s]+/g, (url) => `<a href="${encodeURI(url)}">${url}</a>`);
-  return html;
+  const processor = unified().use(markdown).use(remark2rehype).use(html);
+  const { contents } = await processor.process(src);
+  return contents as string;
 }
