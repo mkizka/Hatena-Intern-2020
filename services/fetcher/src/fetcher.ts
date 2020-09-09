ec3e4ab65096cda96816832cd1879aaf2572d530
mkizka <30231179+mkizka@users.noreply.github.com>
2020-09-09 17:38:31 +0900

    タイトルが取得できなかった場合に対応

--- services/fetcher/src/fetcher.ts
+++ services/fetcher/src/fetcher.ts
@@ -5,7 +5,11 @@ import cheerio from "cheerio";
  * 受け取ったURLからタイトルを取得する
  */
 export async function fetch(url: string): Promise<string> {
-  const response = await request.get(url);
-  const $ = cheerio.load(response.text);
-  return $("title").text();
+  try {
+    const response = await request.get(url);
+    const $ = cheerio.load(response.text);
+    return $("title").text();
+  } catch (e) {
+    return "";
+  }
 }
