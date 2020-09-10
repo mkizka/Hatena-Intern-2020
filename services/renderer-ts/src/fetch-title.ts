3929b11faf87e518ea63ebbcda952964fadf9d79
mkizka <30231179+mkizka@users.noreply.github.com>
2020-09-10 16:52:25 +0900

    Promis.allへの渡し方を変更

--- services/renderer-ts/src/fetch-title.ts
+++ services/renderer-ts/src/fetch-title.ts
@@ -2,23 +2,16 @@ import grpc from "grpc";
 import { FetchRequest } from "../pb/fetcher/fetcher_pb";
 import { FetcherClient } from "../pb/fetcher/fetcher_grpc_pb";
 import { loadConfig } from "./config";
-import { MarkdownLinkNode } from "./unified-utils";
 
-export function fetchTitle(node: MarkdownLinkNode): Promise<void> {
+export function fetchTitle(url: string): Promise<string> {
   const config = loadConfig();
   const fetcherClient = new FetcherClient(config.fetcherAddr, grpc.credentials.createInsecure());
   const fetchRequest = new FetchRequest();
-  fetchRequest.setUrl(node.url);
-  return new Promise<void>((resolve, reject) => {
+  fetchRequest.setUrl(url);
+  return new Promise<string>((resolve, reject) => {
     fetcherClient.fetch(fetchRequest, (err, reply) => {
       if (err) reject(err);
-      else {
-        node.children.push({
-          type: "text",
-          value: reply.getTitle(),
-        });
-        resolve();
-      }
+      else resolve(reply.getTitle());
     });
   });
 }
--- services/renderer-ts/src/remark-auto-title.ts
+++ services/renderer-ts/src/remark-auto-title.ts
@@ -1,14 +1,30 @@
 import { Transformer, Attacher } from "unified";
 import visit from "unist-util-visit";
 import { fetchTitle } from "./fetch-title";
-import { MarkdownLinkNode } from "./unified-utils";
+
+type MarkdownTextNode = {
+  type: string;
+  value: string;
+};
+
+type MarkdownLinkNode = {
+  type: string;
+  url: string;
+  children: MarkdownTextNode[];
+};
 
 const autoTitle: Attacher = () => {
   const transformer: Transformer = async (tree, _) => {
     const promises: Promise<void>[] = [];
     visit<MarkdownLinkNode>(tree, "link", (node) => {
       if (node.children.length === 0) {
-        promises.push(fetchTitle(node));
+        const promise = fetchTitle(node.url).then((title) => {
+          node.children.push({
+            type: "text",
+            value: title,
+          });
+        });
+        promises.push(promise);
       }
     });
     await Promise.all(promises);
